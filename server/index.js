ad3e41cab88bb5679a2d157cf3ef364065953b9a
Rohit <ry164863@gmail.com>
2026-02-21 11:20:12 +0530

    chnage cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,15 @@ import teacherRoutes from "./routes/teacher.routes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://savara-teachers-project.vercel.app",
+        ],
+        credentials: true,
+    })
+);
 app.use(express.json());
 
 connectDB();
